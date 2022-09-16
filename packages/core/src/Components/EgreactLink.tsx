a16217367ba9b69c6980ef48f1cd64eb77259d04
xingfrontend <48762792+xingxinglieo@users.noreply.github.com>
2022-09-16 19:24:57 +0800

    @1.4.x (#22)
    * fix(event): event error
    
    * fix(dist type): dist type
    
    * merge
    
    Co-authored-by: xingyu.hu <xingyu.hu@shopee.com>

--- packages/core/src/Components/EgreactLink.tsx
+++
@@ -1,6 +0,0 @@
-import euiProps from '../Host/eui/index'
-import { Link, LinkProps } from 'react-router-dom'
-import { TransProp } from '../Host'
-import React from 'react'
-
-export const EgreactLink: React.ForwardRefExoticComponent<TransProp<typeof euiProps.label> & LinkProps> = Link as any
