bf6aaf073d0602c916bf4a8e69d1cb2147fde5ea
xingyu.hu <xingyu.hu@shopee.com>
2022-09-16 19:09:54 +0800

    merge

--- packages/dev/src/components/EgreactLink.tsx
+++
@@ -1,6 +0,0 @@
-import { Setters, TransProp } from "egreact";
-import { Link, LinkProps } from "react-router-dom";
-
-export const EgreactLink: React.ForwardRefExoticComponent<
-  TransProp<typeof Setters.eui.label> & LinkProps
-> = Link as any;
